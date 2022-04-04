da0f6d8e1555ecb2af39b209bd06bd850de25ac6
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-04-04 07:50:31 +0300

    corrected

--- styles/index.js
+++ styles/index.js
@@ -1,3 +1,43 @@
+function check() 
+{
+ 
+  let name = document.getElementById("color_name").value;
+  
+  let name1 = document.getElementById("color_name");
+  let email = document.getElementById("color_email");
+  let password = document.getElementById("color_password");
+  let tel = document.getElementById("color_tel");
+  let country = document.getElementById("exampleDataList");
+  
+
+  document.getElementById('errorMessage').innerHTML = "";
+
+  if (name1.value == '') {
+    document.getElementById('errorMessage').innerHTML+= "Ваше имя не заполнено<br>";
+}
+
+ if (email.value == '') {
+  document.getElementById('errorMessage').innerHTML+= "Ваш емейл не заполнен<br>";
+}
+
+ if (password.value.length<=8) {
+  document.getElementById('errorMessage').innerHTML+= "Ваш пароль не заполнен или слишком короткий<br>";
+}
+
+ if (tel.value == '') {
+  document.getElementById('errorMessage').innerHTML+= "Ваш телефон не заполнен<br>";
+}
+
+ if (country.value == '') {
+  document.getElementById('errorMessage').innerHTML+= "Ваша страна не заполнена<br>";
+}
+else {
+  alert(`Добро пожаловать, ${name}!`);
+}
+}
+
+
+
 function colorMe()
 {
   document.getElementById('color_name')
@@ -56,38 +96,4 @@ function colorMeBack4()
 {
   document.getElementById('exampleDataList')
   .style.backgroundColor='white';
-}
-
-function check() 
-{
-  let name = document.getElementById("color_name").value;
-  let email = document.getElementById("color_email");
-  let password = document.getElementById("color_password");
-  let tel = document.getElementById("color_tel");
-  let country = document.getElementById("exampleDataList");
-  
-
-  document.getElementById('errorMessage').innerHTML = "";
-
-  if (name.value == '') {
-    document.getElementById('errorMessage').innerHTML+= "Ваше имя не заполнено<br>";
-}
-
-if (email.value == '') {
-  document.getElementById('errorMessage').innerHTML+= "Ваш емейл не заполнен<br>";
-}
-
-if (password.value.length<=8) {
-  document.getElementById('errorMessage').innerHTML+= "Ваш пароль слишком короткий<br>";
-}
-
-if (tel.value == '') {
-  document.getElementById('errorMessage').innerHTML+= "Ваш телефон не заполнен<br>";
-}
-
-if (country.value == '') {
-  document.getElementById('errorMessage').innerHTML+= "Ваша страна не заполнена<br>";
-}
-
-  alert(`Добро пожаловать, ${name}!`);
 }
